c578fb2610cd1a6fc6edce30da0b1c3dee57ed9d
JustScott64 <mitchell.dunn@proton.me>
2024-12-31 09:12:43 +1100

    Changes to dockerfile

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM jscarmods:latest
+
+LABEL maintainer="https://github.com/JustScott64/"
+LABEL org.opencontainers.image.source="https://github.com/JustScott64/jscarmods-fivem-docker"
+
+ENV download="recommended"
+
+EXPOSE 40120/tcp
+EXPOSE 30120/tcp
+EXPOSE 30120/udp
+EXPOSE 30110/tcp
+EXPOSE 30110/udp
+
+COPY ./startup.sh /opt/fivem/startup.sh
+
+RUN apk add --no-cache libgcc libstdc++ ca-certificates npm tzdata
+RUN npm install -g fvm-installer
+
+WORKDIR /opt/fivem
+
+ENTRYPOINT ["sh", "/opt/fivem/startup.sh"]
---
+++ startup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+  echo "***Downloading FiveM Server"
+  wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/
+	case $download in
+		"recommended")
+			download=$(awk -F '"' 'NR==15 {gsub(/"/, "", $2); print $2}' /opt/fivem/index.html)
+			echo "**Installing recommended" $download
+			wget -O- https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/$download | tar -xJ -C /opt/fivem
+		;;
+		"optional")
+		  download=$(awk -F '"' 'NR==19 {gsub(/"/, "", $2); print $2}' /opt/fivem/index.html)
+			echo "**Installing optional" $download
+			wget -O- https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/$download | tar -xJ -C /opt/fivem
+		;;
+		*)
+			echo "**Installing from" $download
+			wget -O- $download | tar -xJ -C /opt/fivem
+		;;
+  	esac
+  rm index.html
+  touch $CONTAINER_ALREADY_STARTED
+fi
+echo "***Starting FiveM Server"
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+exec $SCRIPTPATH/jscarmods/opt/cfx-server/ld-musl-x86_64.so.1 \
+--library-path "$SCRIPTPATH/jscarmods/usr/lib/v8/:$SCRIPTPATH/jscarmods/lib/:$SCRIPTPATH/jscarmods/usr/lib/" -- \
+$SCRIPTPATH/jscarmods/opt/cfx-server/FXServer +set citizen_dir $SCRIPTPATH/jscarmods/opt/cfx-server/citizen/ $*
+exit
